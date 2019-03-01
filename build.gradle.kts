b6e33b6b1e4979dfe655feb5d457008119acd6a2
bntwng <boranti1995@gmail.com>
2019-03-02 05:47:17 +0900

    build.gradleで動いた

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	id "org.springframework.boot" version "2.1.3.RELEASE"
-	id "org.jetbrains.kotlin.jvm" version "1.2.71"
-	id "org.jetbrains.kotlin.plugin.spring" version "1.2.71"
+	id("org.springframework.boot") version "2.1.3.RELEASE"
+	id("org.jetbrains.kotlin.jvm") version "1.2.71"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.2.71"
 }
 
-apply plugin: "io.spring.dependency-management"
+apply(plugin = "io.spring.dependency-management")
 
 group = "com.wrongwrong"
 version = "0.0.1-SNAPSHOT"
-sourceCompatibility = "1.8"
+val sourceCompatibility = "1.8"
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation "org.springframework.boot:spring-boot-starter"
-	implementation "org.jetbrains.kotlin:kotlin-reflect"
-	implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
-	testImplementation "org.springframework.boot:spring-boot-starter-test"
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-compileKotlin {
+tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs = ["-Xjsr305=strict"]
+		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
 	}
 }
 
-compileTestKotlin {
-	kotlinOptions {
-		freeCompilerArgs = ["-Xjsr305=strict"]
-		jvmTarget = "1.8"
-	}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+	freeCompilerArgs = listOf("-Xjsr305=strict")
+	jvmTarget = "1.8"
 }
