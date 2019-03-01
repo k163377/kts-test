62ec1bdd44fd924c4990e45dae1b33ec904b1931
bntwng <boranti1995@gmail.com>
2019-03-02 06:03:29 +0900

    ハロワした

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation(group = "org.springframework.boot", name = "spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
